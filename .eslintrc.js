04ecc4b5ba42a375417f94180303a6934a20c1ca
Tomina <tomina@vycital.eu>
2022-03-13 14:46:19 +0100

    chore(deps): update ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,34 @@
 module.exports = {
+  extends: [require.resolve("vis-dev-utils/eslint-shareable-config")],
   env: {
-    browser: true,
-    es6: true,
-    node: true,
-    mocha: true
+    commonjs: true,
   },
-
-  parserOptions: {
-    sourceType: "module",
-  },
-
-  extends: "eslint:recommended",
-
-  // For the full list of rules, see: http://eslint.org/docs/rules/
-  rules: {
-    complexity: [2, 55],
-    "max-statements": [2, 115],
-    "no-unreachable": 1,
-    "no-useless-escape": 0,
-
-    "no-console": 0,
-    // To flag presence of console.log without breaking linting:
-    //"no-console": ["warn", { allow: ["warn", "error"] }],
-
-    "require-jsdoc": ["error", {
-        require: {
-          FunctionDeclaration: true,
-          MethodDefinition: true,
-          ClassDeclaration: true,
-          ArrowFunctionExpression: false
-        }
-    }],
-    "valid-jsdoc": [2, {
-      requireReturnDescription: false,
-      requireReturn: false,
-      requireParamDescription: false,
-      requireReturnType: true
-    }],
-    "guard-for-in": 1,
-  },
-}
+  rules: {},
+  overrides: [
+    {
+      files: ["docs/**"],
+      globals: {
+        jQuery: "readonly",
+      },
+      rules: {
+        "no-console": "off",
+        "no-control-regex": "off",
+        "no-prototype-builtins": "off",
+        "no-redeclare": "off",
+        "no-self-assign": "off",
+        "no-sparse-arrays": "off",
+        "no-undef": "off",
+        "no-unused-vars": "off",
+        "no-useless-escape": "off",
+        "no-var": "off",
+        "prefer-const": "off",
+      },
+    },
+    {
+      files: ["docs/js/prettify/**"],
+      globals: {
+        PR: "readonly",
+      },
+    },
+  ],
+};
